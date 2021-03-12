fdbd812096285d9bc5731f9acc7c360ce9eb7f1f
Caique Peixoto <caiquepeixoto1@gmail.com>
2021-03-12 13:01:05 +0100

    Fix App.test.js.

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const comingSoon = screen.getByText(/Coming soon to your favorite app store./i);
+  expect(comingSoon).toBeInTheDocument();
 });
